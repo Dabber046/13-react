7ddf8d25990caf7a87aaabfb38f6cc75d5636c0e
Jared Mindock <jjmin94@gmail.com>
2025-01-27 20:03:37 -0500

    final commit -M "final commit"

---
+++ Develop/src/components/Nav.tsx
@@ -0,0 +1,28 @@
+import { NavLink } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <NavLink 
+            to="/" 
+            className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+          >
+            Candidate Search
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink 
+            to="/SavedCandidates" 
+            className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+          >
+            Saved Candidates
+          </NavLink>
+        </li>
+        </ul>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -0,0 +1,8 @@
+export interface Candidate {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    experience: number; // years of experience
+    skills: string[];
+}
